33b032b539e29a13693ff2de5c527feed9136fd1
Matthew Kennedy <burnsidemk@gmail.com>
2020-02-14 18:44:20 -0600

    Finished challenge

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var storyBrain = StoryBrain()
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    @IBAction func choiceMade(_ sender: UIButton) {
+        switch sender {
+        case choice1Button:
+            storyBrain.choose1()
+            break
+        case choice2Button:
+            storyBrain.choose2()
+            break
+        default:
+            fatalError("Unexpected choice")
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        let currentStory = storyBrain.currentStory()
+        choice1Button.setTitle(currentStory.choice1, for: .normal)
+        choice2Button.setTitle(currentStory.choice2, for: .normal)
+        storyLabel.text = currentStory.title
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
-
-
 }
-
